cabe76a768e465f7e04bb31c382612cecc18974e
Tabish Saiyed <ttab58895@gmail.com>
2025-12-15 22:16:33 +0530

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
+use rand::prelude::*;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    let mut vec = Vec::<Strin>::new();
-    let n = "1";
-    vec.push(n.to_string());
-    let a = "A";
-    assert_eq!("65", a);
-    println!("{}",vec[1]);
+    let rand_num = rand::thread_rng().gen_range(1..10);
+
+    println!("Guess a number!");
+    loop {
+        println!("----------------");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        print!("You Guessed: {guess}");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(val) => val,
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        };
+
+        match guess.cmp(&rand_num) {
+            Ordering::Less => println!("Cold"),
+            Ordering::Greater => println!("Hot"),
+            Ordering::Equal => {
+                println!("You guessed {rand_num} and WON!");
+                break;
+            }
+        }
+    }
 }
